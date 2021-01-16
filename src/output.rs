930d2a06a3c1cc161b433127abb5cb48a06a7217
Heru Handika <herubiolog@gmail.com>
2021-01-16 17:47:24 -0600

    Add results and include id in the csv files.

--- src/output.rs
+++ src/output.rs
@@ -1,13 +1,13 @@
 use std::path::PathBuf;
 use std::fs::File;
-use std::io::{LineWriter, Write};
+use std::io::{LineWriter, Write, Result};
 
-pub fn write_to_csv(recs: &mut [PathBuf], outdir: &str) {
+pub fn write_to_csv(recs: &mut [PathBuf], outdir: &str) -> Result<()> {
     let fname = format!("{}/fastq_records.csv", outdir);
     let csv = File::create(fname).unwrap();
     let mut line = LineWriter::new(csv);
 
-    writeln!(line, "Path,Filenames").unwrap();
+    writeln!(line, "Path,Filenames,Names,Id").unwrap();
 
     recs.sort_by(|a, b| a.cmp(&b));
 
@@ -15,7 +15,18 @@ pub fn write_to_csv(recs: &mut [PathBuf], outdir: &str) {
         .for_each(|r| {
             let path = r.parent().unwrap().to_string_lossy().into_owned();
             let file = r.file_name().unwrap().to_string_lossy().into_owned();
-            writeln!(line, "{},{}", path, file).unwrap();
-        })
+            let name = r.file_stem().unwrap().to_string_lossy().into_owned();
+            let id = split_file_names(&name);
+            writeln!(line, "{},{},{},{}", path, file, name.replace(".fastq", ""), id).unwrap();
+        });
+    
+    Ok(())
 
+}
+
+fn split_file_names(fnames: &String) -> String {
+    let word: Vec<&str> = fnames.split('_').collect();
+    let id = format!("{}_{}_{}", word[0], word[1], word[2]);
+
+    id
 }
