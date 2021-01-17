fe2151c05e6fbe529e5992529146772ac458c1ad
Heru Handika <herubiolog@gmail.com>
2021-01-17 12:09:57 -0600

    Add execution time in the main.

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,17 @@ mod input;
 mod output;
 mod renamer;
 
+use std::time::Instant;
+
 fn main() {
     let version = "0.1.0";
 
+    let tnow = Instant::now();
     cli::get_cli(&version);
+    let elapsed = tnow.elapsed(); 
     
-    println!("Thank you for using renamer v{}", &version);
+    println!("Execution time: {:?}", elapsed);
+    println!("Thank you for using renamer v{} 🙏", &version);
     
 }
 
