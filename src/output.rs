2171061caeebfb9e14d5e51e79c6050f8f5210fd
Heru Handika <herubiolog@gmail.com>
2021-01-17 11:27:32 -0600

    Simplify struct structure. Include all file properties into one struct.

--- src/output.rs
+++ src/output.rs
@@ -13,11 +13,16 @@ pub fn write_to_csv(recs: &mut [PathBuf], outdir: &str) -> Result<()> {
 
     recs.iter()
         .for_each(|r| {
-            let full_path = r.to_string_lossy().into_owned();
-            let parent = r.parent().unwrap().to_string_lossy().into_owned();
-            let file = r.file_name().unwrap().to_string_lossy().into_owned();
-            let id = Id::split_file_names(&file);
-            writeln!(line, "{},{},{},{},{}", full_path,parent, file, id.file_id, id.read_id).unwrap();
+            let mut id = Id::new(&r);
+            id.split_file_names();
+
+            writeln!(line, "{},{},{},{},{}", 
+                id.full_path, 
+                id.parent, 
+                id.fname, 
+                id.file_id, 
+                id.read_id
+            ).unwrap();
         });
 
     println!("Done! The result is saved as {}", &fname);
@@ -25,27 +30,59 @@ pub fn write_to_csv(recs: &mut [PathBuf], outdir: &str) -> Result<()> {
 }
 
 struct Id {
+    full_path: String,
+    parent: String,
+    fname: String,
     file_id: String,
     read_id: String,
 }
 
 impl Id {
-    fn new() -> Self {
+    fn new(lines: &PathBuf) -> Self {
         Self {
+            full_path: lines.to_string_lossy().into_owned(),
+            parent: lines.parent().unwrap().to_string_lossy().into_owned(),
+            fname: lines.file_name().unwrap().to_string_lossy().into_owned(),
             file_id: String::from("N/A"), 
             read_id: String::from("N/A")
         }
     }
 
-    fn split_file_names(fnames: &String) -> Self {
-        let word: Vec<&str> = fnames.split('_').collect();
-        let mut id = Self::new();
+    fn split_file_names(&mut self) {
+        let word: Vec<&str> = self.fname.split('_').collect();
 
         if word.len() == 7 {
-            id.file_id = format!("{}_{}_{}", word[0], word[1], word[2]);
-            id.read_id = format!("{}_{}_{}_{}", word[3], word[4], word[5], word[6]);
+            self.file_id = format!("{}_{}_{}", word[0], word[1], word[2]);
+            self.read_id = format!("{}_{}_{}_{}", word[3], word[4], word[5], word[6]);
         }
+    }
+}
 
-        id
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn path_id_test() {
+        let lines = PathBuf::from("data/HDWND_AAGT_A1_L1_R2_01.fastq.gz");
+        let mut id = Id::new(&lines);
+        id.split_file_names();
+
+        assert_eq!("N/A", id.file_id);
+        assert_eq!("N/A", id.read_id);
+    }
+
+    #[test]
+    fn path_modify_id_test() {
+        let lines = PathBuf::from("data/26535_HDWND_AAGT_A1_L1_R2_01.fastq.gz");
+        let mut id = Id::new(&lines);
+        id.split_file_names();
+
+        assert_eq!("data/26535_HDWND_AAGT_A1_L1_R2_01.fastq.gz", id.full_path);
+        assert_eq!("data", id.parent);
+        assert_eq!("26535_HDWND_AAGT_A1_L1_R2_01.fastq.gz", id.fname);
+        assert_eq!("26535_HDWND_AAGT", id.file_id);
+        assert_eq!("A1_L1_R2_01.fastq.gz", id.read_id);
     }
 }
