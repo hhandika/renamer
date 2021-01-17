0b59a8c079a850f7e88a077fc2de7a2ced7d1500
Heru Handika <herubiolog@gmail.com>
2021-01-16 20:19:17 -0600

    Add method and struct to split filenames.

--- src/output.rs
+++ src/output.rs
@@ -4,29 +4,46 @@ use std::io::{LineWriter, Write, Result};
 
 pub fn write_to_csv(recs: &mut [PathBuf], outdir: &str) -> Result<()> {
     let fname = format!("{}/fastq_records.csv", outdir);
-    let csv = File::create(fname).unwrap();
+    let csv = File::create(&fname).unwrap();
     let mut line = LineWriter::new(csv);
 
-    writeln!(line, "Path,Filenames,Names,Id").unwrap();
+    writeln!(line, "full_path,filenames,id,read_id").unwrap();
 
     recs.sort_by(|a, b| a.cmp(&b));
 
     recs.iter()
         .for_each(|r| {
-            let path = r.parent().unwrap().to_string_lossy().into_owned();
+            let full_path = r.to_string_lossy().into_owned();
             let file = r.file_name().unwrap().to_string_lossy().into_owned();
-            let name = r.file_stem().unwrap().to_string_lossy().into_owned();
-            let id = split_file_names(&name);
-            writeln!(line, "{},{},{},{}", path, file, name.replace(".fastq", ""), id).unwrap();
+            let id = Id::split_file_names(&file);
+            writeln!(line, "{},{},{},{}", full_path, file, id.file_id, id.read_id).unwrap();
         });
-    
+    println!("Done! The result is saved as {}", &fname);
     Ok(())
-
 }
 
-fn split_file_names(fnames: &String) -> String {
-    let word: Vec<&str> = fnames.split('_').collect();
-    let id = format!("{}_{}_{}", word[0], word[1], word[2]);
+struct Id {
+    file_id: String,
+    read_id: String,
+}
 
-    id
+impl Id {
+    fn new() -> Self {
+        Self {
+            file_id: String::from("N/A"), 
+            read_id: String::from("N/A")
+        }
+    }
+
+    fn split_file_names(fnames: &String) -> Self {
+        let word: Vec<&str> = fnames.split('_').collect();
+        let mut id = Self::new();
+
+        if word.len() == 7 {
+            id.file_id = format!("{}_{}_{}", word[0], word[1], word[2]);
+            id.read_id = format!("{}_{}_{}_{}", word[3], word[4], word[5], word[6]);
+        }
+
+        id
+    }
 }
