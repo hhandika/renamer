4d9ae1db5c4516cac16b9a1f3582844d27c891b1
Heru Handika <herubiolog@gmail.com>
2021-01-26 11:20:06 -0600

    Remove dry run unused code.

--- src/dryrun.rs
+++ src/dryrun.rs
@@ -6,17 +6,6 @@ use crate::parser;
 pub fn dry_run(path: &str) -> Result<(), Error> {
     let dryrun = true;
     parser::parse_csv(path, dryrun);
-    // let mut errors = 0;
-
-    // println!("Checking files...");
-    // filenames.iter()
-    //     .for_each(|(old, new)| {
-    //         checker::check_input_errors(old, new, &mut errors);
-    //     });
-
-    // if errors > 0 {
-    //     checker::display_errors(&errors);
-    // }
 
     Ok(())
 }
